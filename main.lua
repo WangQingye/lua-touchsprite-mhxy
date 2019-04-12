54b12209b6a55e418eb079b36e88842628e8652c
wangqingye <wangqingye@cetcxl.com>
2019-04-12 17:58:43 +0800

    shimen

--- main.lua
+++ main.lua
@@ -1,9 +1,12 @@
 require("TSLib")
 require("utilfind")
 -- 当前是否有师门任务
-local hasShimen = false;
--- 当前是否在循环查找师门按钮（为了防止多线程重复查找）
-local doingShimen = false;
+local hasShimen = true;
+-- 这两个变量的作用是用来标志查找次数，因为在师门的任务间隙，有可能第一次找不到，如果两次间隔10秒都找不到，才证明确实没有
+local findShimen1 = true;
+local findShimen2 = true;
+local findShimen3 = true;
+local findShimen4 = true;
 
 function findShimenTextByOcr()	
 	for i = 1, 3 do
@@ -21,45 +24,103 @@ function findShimenTextByColor()
 		local y1 = 228 + (i-1) * 120
 		local y2 = 268 + (i-1) * 120
 		local x, y = findMultiColorInRegionFuzzy( 0xf3eb20, "14|8|0xe5de24,34|14|0xe9e221", 85, 1294, y1, 1358, y2)
-		if x > -1 and y > -1 then
+		if x ~= nil and y > -1 then
 			return 1296 + 50, y1 + 50
 		end
 	end
 end
 -- 查看是否有师门任务
 function testShimen()
-	toast("开始师门任务",2)
 	-- 师门找到过后理论上位置不会再变，而且做一个师门需要花费的时间比较长，所以师门的点击等待时间可以比较长
 	local a, b = findShimenTextByColor();
-	if a  > -1 and b  > -1 then
+	if a ~= nil and b > -1 then
 		hasShimen = true;
+		findShimen1 = true;
+		findShimen2 = true;
+		findShimen3 = true;		
+		findShimen4 = true;
 		randomTap(a,b,5);
-		if doingShimen == false then
-			doShimen();
-		end
-		-- 10秒后再次执行startShimen看看是否还有师门任务，如果有的话就点击
-		mSleep(10000);
-		testShimen();
+		doShimen();
 	else
-		hasShimen = false;
-		toast("没有找到师门二字",2)
+		if findShimen1 == true then
+			findShimen1 = false
+			toast("一次没有找到师门",2)
+		elseif findShimen2 == true then
+			findShimen2 = false
+			toast("两次没有找到师门",2)
+		elseif findShimen3 == true then
+			findShimen3 = false
+			toast("三次没有找到师门",2)
+		elseif findShimen4 == true then
+			findShimen4 = false
+		end		
+		if findShimen1 == false and findShimen2 == false then
+			if findShimen3 == false and findShimen4 == false then
+				hasShimen = false;
+				toast("四次没有找到师门，师门结束",2)
+			end
+		end
 	end
 end
 -- 师门情况分类处理
--- 采集 - 使用 - 1
--- 巡逻 - 自动 - 2
 function doShimen()
 	toast("开始查找师门按钮",2)
-	doingShimen = true;
 	-- 有师门的时候才会去循环查找师门任务按钮
+	local findShimenBtn1 = true;	
+	local findShimenBtn2 = true;
+	local findShimenBtn3 = true;
+	local findShimenBtn4 = true;
 	while (hasShimen) do
+		local isFind = false;
 		mSleep(5000);
-		local retTable = findUtil("使用");
-		if retTable ~= false then
-			toast("找到使用按钮",2);
-			randomTap(retTable[1],retTable[2],5);			
-		end		
+		local shimenText = {"使用","购买","上交","兵器购买","兵器购买上交","选择要做师门1","选择要做师门2","选择要做师门3","选择要做师门4","送信对话框"};
+		local retTable;
+		for i,v in ipairs(shimenText) do
+			retTable = findUtil(v);
+			if retTable ~= false then
+				randomTap(retTable[1],retTable[2],5);
+				toast("找到按钮："..v,2);
+				if (v == "购买" or v == "兵器购买") then
+					isFind = false;
+				else
+					isFind = true;
+					break;
+				end				
+			end
+		end
+		if retTable == false then
+			if findShimenBtn1 == true then
+				findShimenBtn1 = false
+				toast("一次没有找到师门按钮",2)
+			elseif findShimenBtn2 == true then
+				findShimenBtn2 = false
+				toast("两次没有找到师门按钮",2)
+			elseif findShimenBtn3 == true then
+				findShimenBtn3 = false
+				toast("三次没有找到师门按钮",2)
+			elseif findShimenBtn4 == true then
+				findShimenBtn4 = false
+			end		
+			if findShimenBtn1 == false and findShimenBtn2 == false then
+				if findShimenBtn3 == false and findShimenBtn4 == false then
+					toast("这不是一个需要点击的师门任务",2)
+					break;
+				end
+			end
+		end
+		if isFind then
+			break;
+		end
 	end
 end
 
-testShimen();
+while (true) do
+	toast("准备开始师门",2);	
+	if hasShimen == false then
+		break;
+	else
+		testShimen();
+	end
+	mSleep(15000);
+end
+
--- utilfind.lua
+++ utilfind.lua
@@ -2,7 +2,7 @@
 function findUtil(name)
 	table = utilPosition[name]
 	local a, b = findMultiColorInRegionFuzzy(table[1], table[2], table[3], table[4], table[5], table[6], table[7])
-	if a > -1 and b > -1 then
+	if a ~= nil and b > -1 then
 		return {a,b}
 	else
 		return false
@@ -12,3 +12,12 @@ end
 utilPosition = {}
 -- 使用
 utilPosition["使用"] = {0x6c310a, "13|17|0x70350c,-2|34|0xedba48", 90, 1368, 697, 1414, 746}
+utilPosition["购买"] = {0xfffefe, "0|-17|0xf3ece7,7|-7|0xedb94f", 90, 1297, 785, 1350, 831}
+utilPosition["上交"] = {0x6f340c, "-1|8|0xf7d36d,0|20|0x6c310a", 90, 1062, 713, 1098, 749}
+utilPosition["送信对话框"] = {0xe7b578, "-27|-9|0xf4cfa5,-43|-36|0xf8d4ab", 90, 402, 827, 474, 888}
+utilPosition["选择要做师门1"] = {0x6f350e, "5|0|0xf0dbbd,11|6|0x6f350e", 90, 1344, 314, 1368, 336}
+utilPosition["选择要做师门2"] = {0x6f350e, "5|0|0xf0dbbd,11|6|0x6f350e", 90, 1344, 398, 1368, 420}
+utilPosition["选择要做师门3"] = {0x6f350e, "5|0|0xf0dbbd,11|6|0x6f350e", 90, 1344, 482, 1368, 504}
+utilPosition["选择要做师门4"] = {0x6f350e, "5|0|0xf0dbbd,11|6|0x6f350e", 90, 1344, 566, 1368, 588}
+utilPosition["兵器购买"] = {0xffffff, "-13|17|0xc17b36,19|8|0xf9d275", 90, 1078, 682, 1139, 738}
+utilPosition["兵器购买上交"] = {0x6e340c, "-1|10|0xf6d069,-1|19|0x6e330b", 90, 1319, 713, 1356, 751}
